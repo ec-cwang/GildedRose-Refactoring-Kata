64f071bc4bbc3a56ff8d627c9ef476df5e72f4f8
ec-cwang <chen.wang.ec@gmail.com>
2017-03-04 21:55:36 +0100

    Change first test name

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
+    @Test public void
+    should_have_right_item_name_when_created_by_the_constructor() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
